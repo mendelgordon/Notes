a2ded9af9b79ec55d883fcd45929b8cd840f02c9
Mendel Gordon <mendelgordons@gmail.com>
2023-06-29 10:55:55 -0400

    Remove appwrite - plan to use firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import { Client } from "appwrite";
-const client = new Client();
-
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject("649cedbded595779b96e");
-
 export const App = () => {
 	return (
 		<div>
