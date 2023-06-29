267d2abb6f09723b0dd90a06ec21e3513e486b91
Mendel Gordon <mendelgordons@gmail.com>
2023-06-29 15:01:45 -0400

    Configure firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAv0Ym6pqkZDfM2BTjXDgGOz6VVlJV3djs",
+  authDomain: "notes-70da2.firebaseapp.com",
+  projectId: "notes-70da2",
+  storageBucket: "notes-70da2.appspot.com",
+  messagingSenderId: "831679024451",
+  appId: "1:831679024451:web:cadacc4a0dfedbc04db3f6"
+};
+
+const app = initializeApp(firebaseConfig);
