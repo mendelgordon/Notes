1bfe45d941ce08f5fd807266eec9214b8425dad7
Mendel Gordon <mendelgordons@gmail.com>
2023-07-04 13:44:19 -0400

    Add css back

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import htmlPurge from "vite-plugin-html-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr(), htmlPurge.default()],
+	plugins: [react(), svgr()],
 });
