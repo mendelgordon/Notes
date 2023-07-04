b95f814b324ee15e4dccd4e3d1ee890e8e30231a
Mendel Gordon <mendelgordons@gmail.com>
2023-07-04 13:34:05 -0400

    Remove purgecss since it removed too much

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import htmlPurge from "vite-plugin-html-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()],
+	plugins: [react(), svgr(), htmlPurge.default()],
 });
